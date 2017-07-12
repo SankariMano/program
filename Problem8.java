315eb6852061ad56ee23611e813f2a58d468b27d
SankariMano <sankarimano97@gmail.com>
2017-07-12 20:55:49 +0530

    Create Problem8.java

---
+++ Problem8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Problem8{
+    public static void main(String[] args) throws IOException {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       List<Integer> x=new<Integer> ArrayList();
+       Integer y;
+                for(int i=100;i<=999;i++)
+               {
+                  if((i%6)==0) {
+                     y=i;
+                     x.add(y) ;
+                  }
+               }
+         System.out.println(x);
+ }
+    
+}

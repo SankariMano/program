515d2d4ed53f86713280040b05dfc0c758314bbe
SankariMano <sankarimano97@gmail.com>
2017-07-12 22:03:27 +0530

    Create Problem11.java

---
+++ Problem11.java
@@ -0,0 +1,23 @@
+              import java.io.*;
+import java.util.*;
+public class Problem11{
+    public static void main(String[] args) throws IOException {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      
+       
+        Integer distance,speed,speed1,total,time;
+        System.out.println("Speed");
+        speed1=Integer.parseInt(br.readLine());
+        System.out.println("time");        
+         time=Integer.parseInt(br.readLine());
+       speed=(speed1*1000)/60;
+       distance=(speed*time)/2;
+       
+        
+              
+               System.out.println(total);
+                              
+ }
+  
+}
+

35866df53a7ce80b5b5f945ef53f1cdd1e16b6df
SankariMano <sankarimano97@gmail.com>
2017-07-12 21:44:04 +0530

    Create Program12.java

---
+++ Program12.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Problem12 {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      
+       
+        Integer product,sum;
+        sum=138;
+        product=131;
+        Double total=Math.sqrt(sum+(2*product));
+        
+              
+               System.out.println(total);
+                              
+ }
+  
+}

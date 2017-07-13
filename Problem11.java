76908d0e449c26dd3d8761364ae3e6684bcb25e1
SankariMano <sankarimano97@gmail.com>
2017-07-13 12:37:43 +0530

    Update Problem11.java

--- Problem11.java
+++ Problem11.java
@@ -5,13 +5,17 @@ public class Problem11{
        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
       
        
-        Integer distance,speed,speed1,total,time;
+        Integer distance,speed,speed1,total,time,l,b;
         System.out.println("Speed");
         speed1=Integer.parseInt(br.readLine());
         System.out.println("time");        
          time=Integer.parseInt(br.readLine());
        speed=(speed1*1000)/60;
        distance=(speed*time)/2;
+      Integer x=distance/10;
+      l=3*x;
+      b=2*x;
+      Integer total=l*b;
        
         
               
